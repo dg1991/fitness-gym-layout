a9f5d4a309ff8d24bb0b2092b3bb340e3f7cc65d
Denis Mauricio Gaurón <denismauriciogauron@gmail.com>
2021-04-10 20:14:54 -0300

    Versión 1.0 Sitio de Fitness

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    $('.menu-icon').on('click', function(){
+        $('nav').slideToggle();
+    });
+
+});
+
+const video = document.getElementById('video');
+const boton =document.getElementById('boton');
+
+function playVideo() {
+    video.play();
+    boton.style.display = 'none';
+}
+
+video.addEventListener('ended', function() {
+    boton.style.display = 'block';
+});
